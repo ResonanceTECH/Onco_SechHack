9c19ec38d86537c166bf04da294e477b50bbe299
Daria Chugunova <dariachugunova2003@gmail.com>
2026-02-26 15:02:57 +0300

    refactor(db): улучшение утилиты DBManager и работы с сессиями

--- backend/src/utils/db_manager.py
+++ backend/src/utils/db_manager.py
@@ -1,14 +1,5 @@
-"""from src.repositories.bookings import BookingsRepository"""
 from src.repositories.cases import CasesRepository
 from src.repositories.users import UsersRepository
-from src.repositories.items import ItemsRepository
-from src.repositories.payments import PaymentsRepository
-
-"""from src.repositories.facilities import FacilitiesRepository, RoomsFacilitiesRepository
-from src.repositories.hotels import HotelsRepository
-from src.repositories.rooms import RoomsRepository
-
-from src.repositories.users import UsersRepository"""
 
 
 class DBManager:
@@ -17,17 +8,8 @@ class DBManager:
 
     async def __aenter__(self):
         self.session = self.session_factory()
-
-        """self.hotels = HotelsRepository(self.session)
-        self.rooms = RoomsRepository(self.session)
-        self.users = UsersRepository(self.session)
-        self.bookings = BookingsRepository(self.session)
-        self.facilities = FacilitiesRepository(self.session)
-        self.rooms_facilities = RoomsFacilitiesRepository(self.session)"""
-        self.items = ItemsRepository(self.session)
         self.cases = CasesRepository(self.session)
         self.users = UsersRepository(self.session)
-        self.payments = PaymentsRepository(self.session)
 
 
         return self
